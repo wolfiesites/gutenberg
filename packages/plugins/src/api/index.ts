5c64fd08752f4631766284cb13a2b73c18dbe75a
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-08-09 21:25:16 +0900

    Add notes about edit-site

--- packages/plugins/src/api/index.ts
+++ packages/plugins/src/api/index.ts
@@ -55,6 +55,7 @@ const plugins = {} as Record< string, WPPlugin >;
  * // Using ES5 syntax
  * var el = wp.element.createElement;
  * var Fragment = wp.element.Fragment;
+ * // In the site editor, import PluginSidebar and PluginSidebarMoreMenuItem from wp.editSite instead.
  * var PluginSidebar = wp.editPost.PluginSidebar;
  * var PluginSidebarMoreMenuItem = wp.editPost.PluginSidebarMoreMenuItem;
  * var registerPlugin = wp.plugins.registerPlugin;
@@ -91,6 +92,8 @@ const plugins = {} as Record< string, WPPlugin >;
  * @example
  * ```js
  * // Using ESNext syntax
+ *
+ * // In the site editor, import PluginSidebar and PluginSidebarMoreMenuItem from @wordpress/edit-site instead.
  * import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
  * import { registerPlugin } from '@wordpress/plugins';
  * import { more } from '@wordpress/icons';
