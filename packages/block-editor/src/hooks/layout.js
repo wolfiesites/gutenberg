6d48c26ee5e40123858e284abb8792f35a7f8def
tellthemachines <isabel@tellthemachines.com>
2023-08-18 16:32:45 +1200

    Fix matrix resetting values

--- packages/block-editor/src/hooks/layout.js
+++ packages/block-editor/src/hooks/layout.js
@@ -433,11 +433,29 @@ function LayoutPanel( { setAttributes, attributes, name: blockName } ) {
 
 	const onChangeMatrix = ( newValue ) => {
 		const [ horizontal, vertical ] = newValue.split( ' ' );
+		let alignment = horizontal;
+		let justification = vertical;
+
+		if (
+			verticalAlignment === 'stretch' ||
+			verticalAlignment === 'space-between'
+		) {
+			setMatrixAlignment( horizontal );
+			alignment = verticalAlignment;
+		}
+
+		if (
+			justifyContent === 'stretch' ||
+			justifyContent === 'space-between'
+		) {
+			setMatrixJustification( vertical );
+			justification = justifyContent;
+		}
 
 		onChangeLayout( {
 			...usedLayout,
-			justifyContent: vertical,
-			verticalAlignment: horizontal,
+			justifyContent: justification,
+			verticalAlignment: alignment,
 		} );
 	};
 
